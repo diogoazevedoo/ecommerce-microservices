bd2de80cc19ad7456114a5e7054293bb2225e4ea
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-07-03 21:56:44 +0100

    feat(discovery): add spring initializer template for discovery server

---
+++ services/discovery/src/test/java/com/ecommerce_microservices/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_microservices.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
